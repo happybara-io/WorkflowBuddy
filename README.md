69e83e0e5783ac9bf10492c8fc40959b83f19cbc
Kevin Quinn <kpquinn2@wisc.edu>
2022-09-03 17:12:47 -0500

    Demo example Workflow from Slack docs

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Slack Automation - Local Bolt Project
 
 Testing locally with Bolt and Ngrok to try learning some new things.
+
+
+## Steps to run
+
+- `poetry shell` so all our environment variables are easy
+- (in a separate terminal) Run `./ngrok` to get a public domain address - [Ngrok Dashboard](https://dashboard.ngrok.com)
+- Run the local server with `./run.sh`
+- Update the Slack App console with new address - for [Event Subscriptions](https://api.slack.com/apps/A040W1RHGBX/event-subscriptions?), Interactivity
+
+## Notes during setup of demo
+
+- [Slack Guide to get setup over HTTP](https://slack.dev/bolt-python/tutorial/getting-started-http)
+- Demo requirements doesn't even include their own package, confusing
+- Slack bolt uses same `/slack/events` path by default for both Events and Interactivity
--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import logging
+import requests
+import random
 
 logging.basicConfig(level=logging.DEBUG)
 
 from slack_bolt import App
+from slack_bolt.workflows.step import WorkflowStep
 from slack_bolt.adapter.flask import SlackRequestHandler
 
 app = App()
 
+###########################
+# Utilities
+############################
+def send_webhook(url, json_body):
+    resp = requests.post(url, json=json_body)
+    print(f'{resp.status_code}: {resp.text}')
 
+
+###########################
+# Slack app stuff
+############################
 @app.middleware  # or app.use(log_request)
 def log_request(logger, body, next):
     logger.debug(body)
@@ -24,7 +37,85 @@ def event_test(body, say, logger):
 def handle_message():
     pass
 
+def edit(ack, step, configure):
+    ack()
 
+    blocks = [
+        {
+            "type": "input",
+            "block_id": "task_name_input",
+            "element": {
+                "type": "plain_text_input",
+                "action_id": "name",
+                "placeholder": {"type": "plain_text", "text": "Add a task name"},
+            },
+            "label": {"type": "plain_text", "text": "Task name"},
+        },
+        {
+            "type": "input",
+            "block_id": "task_description_input",
+            "element": {
+                "type": "plain_text_input",
+                "action_id": "description",
+                "placeholder": {"type": "plain_text", "text": "Add a task description"},
+            },
+            "label": {"type": "plain_text", "text": "Task description"},
+        },
+    ]
+    configure(blocks=blocks)
+
+def save(ack, view, update):
+    ack()
+
+    values = view["state"]["values"]
+    task_name = values["task_name_input"]["name"]
+    task_description = values["task_description_input"]["description"]
+                
+    inputs = {
+        "task_name": {"value": task_name["value"]},
+        "task_description": {"value": task_description["value"]}
+    }
+    outputs = [
+        {
+            "type": "text",
+            "name": "task_name",
+            "label": "Task name",
+        },
+        {
+            "type": "text",
+            "name": "task_description",
+            "label": "Task description",
+        }
+    ]
+    update(inputs=inputs, outputs=outputs)
+
+
+def execute(step, complete, fail):
+    inputs = step["inputs"]
+    # if everything was successful
+    outputs = {
+        "task_name": inputs["task_name"]["value"],
+        "task_description": inputs["task_description"]["value"],
+    }
+    complete(outputs=outputs)
+
+    # if something went wrong
+    error = {"message": "Just testing step failure!"}
+    fail(error=error)
+
+# Create a new WorkflowStep instance
+ws = WorkflowStep(
+    callback_id="demo",
+    edit=edit,
+    save=save,
+    execute=execute,
+)
+# Pass Step to set up listeners
+app.step(ws)
+
+###########################
+# Flask app stuff
+############################
 from flask import Flask, request
 
 flask_app = Flask(__name__)
